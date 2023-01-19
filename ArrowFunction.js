d9da27e53ed3d9384cdf50924375301324e0534c
RahulAid <raccess97@gmail.com>
2023-01-19 14:06:58 +0530

    Arrow Function

---
+++ ArrowFunction.js
@@ -0,0 +1,9 @@
+// Arrow Function
+
+const add = (a, b) => { return a + b};
+
+const res = add(1, 2); // 3
+
+// If the syntax is simple, `{}` and `return` can be omitted. It will look neater
+const minus = (a, b) => a - b;
+const res1 = minus(3, 1); // 2
