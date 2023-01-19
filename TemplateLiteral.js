37d6476fde6a5ca5e675a53063f761b13c7a610e
RahulAid <raccess97@gmail.com>
2023-01-19 14:27:19 +0530

    Template Literal

---
+++ TemplateLiteral.js
@@ -0,0 +1,8 @@
+// Template Literal
+
+const firstName = 'Ken';
+const lastName = 'Huang';
+// not use template literal
+const name = 'Hello, My name is' + firstName + ', ' + lastName;
+// use template literal
+const nameWithLiteralString = `Hello, My name is ${firstName}, ${lastName}`;
